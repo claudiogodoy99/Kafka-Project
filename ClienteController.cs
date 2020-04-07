e7ed18f44a894d52c3c7bb37216bd673c8ec09c8
claudio godoy <popo_dvs2009@hotmail.com>
2020-04-07 00:25:39 -0300

    initial play

---
+++ ClienteController.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Produtor;
+
+namespace Demostrativa
+{
+    [Route("api/[controller]")]
+    public class ClienteController : Controller
+    {
+        private readonly ILogger<EntidadeCliente> _logger;
+
+        public ClienteController(ILogger<EntidadeCliente> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody]EntidadeCliente entidade)
+        {
+            try
+            {
+                Logar(entidade);
+
+                Produtor<EntidadeCliente> produtor = new Produtor<EntidadeCliente>(_logger);
+                var taskPorduto = produtor.Produzir(entidade);
+              
+
+                await taskPorduto;
+
+                return Created("api/cliente",entidade.cpf) ;
+            }
+            catch 
+            {
+                return Problem();
+
+            }
+        }
+
+        public async void Logar(EntidadeCliente cliente) 
+        {
+            _logger.LogInformation($"criar salvar cliente: {JsonConvert.SerializeObject(cliente)}");
+        }
+    }
+}
